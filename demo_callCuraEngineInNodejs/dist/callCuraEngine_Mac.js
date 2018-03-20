77dd8058ed26d7ceb8a7ad680c9cc176ac102df5
liuming <374406742@qq.com>
2018-03-20 11:38:07 +0800

    a demo of calling CuraEngine using Nodejs

---
+++ demo_callCuraEngineInNodejs/dist/callCuraEngine_Mac.js
@@ -0,0 +1,71 @@
+"use strict";
+
+var child_process = require('child_process');
+var spawn = child_process.spawn;
+
+var sliceProgress, filamentLength, filamentWeight, printTime;
+
+var curaEnginePath = "../CuraEngine/CuraEngine";
+var configPath = "../CuraEngine/Config/normal_quality.def.json";
+var gcodePath;
+var stlPath = "../test.stl";
+
+//exec "$curaEnginePath" slice -v -p -j "$configPath" -o "$gcodePath" -l "$stlPath"
+
+gcodePath = getGcodePath();
+var wmic = spawn(curaEnginePath, ['slice', '-v', '-p', '-j', configPath, '-o', gcodePath, '-l', stlPath]);
+
+wmic.stdout.on('data', function (data) {});
+
+wmic.stderr.on('data', function (data) {
+
+	var array = data.toString().split('\n');
+
+	array.map(function (item) {
+
+		if (item.length < 10) {
+			return;
+		}
+
+		if (item.indexOf('Progress:inset+skin:') === 0 || item.indexOf('Progress:export:') === 0) {
+
+			var start = item.indexOf('0.');
+			var end = item.indexOf('%');
+			sliceProgress = Number(item.slice(start, end));
+			console.log('slice progress = ' + sliceProgress);
+		} else if (item.indexOf(';Filament used:') === 0) {
+
+			filamentLength = Number(item.replace(';Filament used:', '').replace('m', ''));
+			filamentWeight = Math.PI * (1.75 / 2) * (1.75 / 2) * filamentLength * 1.24;
+			sliceProgress = 1;
+		} else if (item.indexOf('Print time:') === 0) {
+
+			//add empirical parameter : 1.07
+			printTime = Number(item.replace('Print time:', '')) * 1.07;
+			sliceProgress = 1;
+		}
+	});
+});
+
+wmic.on('close', function (code) {
+
+	console.log('**************************************');
+
+	//0:ok  1:error
+	console.log('child process exited with code ' + code);
+
+	console.log('slice progress = ' + sliceProgress);
+	console.log('filamentLength = ' + filamentLength);
+	console.log('filamentWeight = ' + filamentWeight);
+	console.log('printTime = ' + printTime);
+
+	console.log('**************************************');
+});
+
+function getGcodePath() {
+
+	var date = new Date();
+	var gcodeFileName = date.getHours() + "-" + date.getMinutes() + "-" + date.getSeconds();
+	return "../" + gcodeFileName + ".gcode";
+}
+//# sourceMappingURL=callCuraEngine_Mac.js.map
